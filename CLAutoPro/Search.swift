b0093b0e5ea2a6b8e7edf36a468429e1497318db
Chris Wineland <chriswineland01@gmail.com>
2014-11-06 10:06:31 -0800

    created the base for the search object

--- CLAutoPro/Search.swift
+++ CLAutoPro/Search.swift
@@ -8,6 +8,28 @@
 
 import Foundation
 
-class Search {
+class Search{
+    
+    var creationDate = NSDate()
+    var lastQuieryDate = NSDate()
+    //required
+    var location = [String]()
+    var make, model : String?
+    //optional
+    var yearMin, yearMax, priceMin, priceMax, milesMin, milesMax : Int?
+    var color : String?
+    var transmission, hasImage, hasTitle : Bool?
+    
+    init() {
+        
+    }
+    
+    func isValidSearch() -> Bool{
+        return location.count > 0 && make != nil && model != nil
+    }
+    
+    func quieryString() -> String{
+        return ""
+    }
     
 }

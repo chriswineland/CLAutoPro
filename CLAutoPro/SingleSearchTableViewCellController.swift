c91f4ad6ec6dd2eac8c449788785a978823346ca
Chris Wineland <chriswineland01@gmail.com>
2014-11-09 11:19:15 -0800

    added additional file structure for views added the view for the searches tableview cell

---
+++ CLAutoPro/SingleSearchTableViewCellController.swift
@@ -0,0 +1,14 @@
+//
+//  SingleSearchTableViewCellController.swift
+//  CLAutoPro
+//
+//  Created by Chris Wineland on 11/9/14.
+//  Copyright (c) 2014 Chris Wineland. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SingleSearchTableViewCellController {
+    
+}
